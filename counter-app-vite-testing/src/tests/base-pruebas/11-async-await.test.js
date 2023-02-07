e9718d9a3b697993f9531e2d6f81bcbf54a14998
ST-Alan <contacto@alanfermin.com>
2023-02-07 03:06:24 -0500

    Fin testing con JS

--- counter-app-vite-testing/src/tests/base-pruebas/11-async-await.test.js
+++ counter-app-vite-testing/src/tests/base-pruebas/11-async-await.test.js
@@ -1,13 +1,20 @@
-import { getImagen } from '../../base-pruebas/11-async-await'
+import { getImagen, getImagenSinApiKey } from '../../base-pruebas/11-async-await'
 
 describe('Pruebas en 11-async-await.test', () => {
 
 
-    test('getImagen debe retornar un url de la imagen', () => {
+    test('getImagen debe retornar un url de la imagen', async () => {
 
         const url = await getImagen()
         console.log(url)
         expect(typeof url).toBe('string')
+
+    })
+    test('getImagenSinApiKey debe retornar el error del catch', async () => {
+
+        const resp = await getImagenSinApiKey()
+        console.log(resp)
+        expect(resp).toBe('No se encontró la imagen')
 
     })
 
